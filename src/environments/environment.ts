ba5a16897f571acacbb7ca7c5cdddf6143a334d0
Kornchanok Iednusorn <kornchanokiednusorn@gmail.com>
2025-05-11 01:02:50 +0700

    vercel fix

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,8 @@
+import { Injectable } from '@angular/core';
+
+// Angular environment file
 export const environment = {
   production: false,
-  supabaseUrl: process.env['SUPABASE_URL'] || '',
-  supabaseKey: process.env['SUPABASE_ANON_KEY'] || '',
+  supabaseUrl: '${process.env["SUPABASE_URL"]}',
+  supabaseKey: '${process.env["SUPABASE_ANON_KEY"]}',
 };
