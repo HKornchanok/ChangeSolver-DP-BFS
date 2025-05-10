0183e78a176f152269ad2f1e67f834c82e0943b3
Kornchanok Iednusorn <kornchanokiednusorn@gmail.com>
2025-05-11 01:18:05 +0700

    anon key

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,6 @@
-import { Injectable } from '@angular/core';
-
-// Angular environment file
 export const environment = {
   production: false,
-  supabaseUrl: '${process.env["SUPABASE_URL"]}',
-  supabaseKey: '${process.env["SUPABASE_ANON_KEY"]}',
+  supabaseUrl: 'https://gbmkxsgpqumfhhvhcpng.supabase.co',
+  supabaseKey:
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdibWt4c2dwcXVtZmhodmhjcG5nIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY4NjIyNDksImV4cCI6MjA2MjQzODI0OX0.jrrwtXxbUDg8ZvTqYpJr-KkK5U0qMnWmpNeTieYjBeI',
 };
